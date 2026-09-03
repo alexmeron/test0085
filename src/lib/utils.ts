736c0a830fb248a9661267dc20ebd0259cad8a11
alexmeron <81044772+alexmeron@users.noreply.github.com>
2026-09-03 09:51:45 +0200

    fix(utils): extend tailwind-merge configuration to prevent it from stripping custom Figma variables

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,27 @@
 import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+import { figmaTheme } from '../styles/tailwind-theme.js'
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [{ text: Object.keys(figmaTheme.fontSize) }],
+      'text-color': [{ text: Object.keys(figmaTheme.colors) }],
+      'bg-color': [{ bg: Object.keys(figmaTheme.colors) }],
+      'border-color': [{ border: Object.keys(figmaTheme.colors) }],
+      'rounded': [{ rounded: Object.keys(figmaTheme.borderRadius) }],
+      'p': [{ p: Object.keys(figmaTheme.spacing) }],
+      'px': [{ px: Object.keys(figmaTheme.spacing) }],
+      'py': [{ py: Object.keys(figmaTheme.spacing) }],
+      'w': [{ w: Object.keys(figmaTheme.spacing) }],
+      'h': [{ h: Object.keys(figmaTheme.spacing) }],
+      'font-family': [{ font: Object.keys(figmaTheme.fontFamily) }],
+      'font-weight': [{ font: Object.keys(figmaTheme.fontWeight) }],
+      'leading': [{ leading: Object.keys(figmaTheme.lineHeight) }],
+      'shadow': [{ shadow: Object.keys(figmaTheme.boxShadow) }]
+    }
+  }
+})
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
